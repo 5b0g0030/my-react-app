4385f3977eb6b0eab17c4d21607bce1fe12160a4
5b0g0030 <5b0g0030@office.stust.edu.tw>
2024-05-29 20:31:13 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import MyRGBPanel from "./components/MyRGBPanel";
 import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
     <div className="App">
-      <h1>5b0g0030 仇子昕 第二元件</h1>
+      <h1>MySlider</h1>
       <MySlider />
       <h1>MyRGBPanel</h1>
       <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
     </div>
   );
 }
