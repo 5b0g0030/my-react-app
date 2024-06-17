67ab2e2d704c0f79d12c74ddedf87caf479da68f
5b0g0030 <5b0g0030@office.stust.edu.tw>
2024-06-18 01:43:34 +0800

    fix

--- src/components/MyRGBPanel.js
+++ src/components/MyRGBPanel.js
@@ -1,51 +1,56 @@
-import React from "react";
-import { useState } from "react";
-import MySlider from "./MySlider";
+import React, { useState } from "react"; // 從 React 模組中引入 React 和 useState
+import MySlider from "./MySlider"; // 引入 MySlider 組件
 
+// 定義 MyRGBPanel 組件
 function MyRGBPanel() {
+    // 使用 useState 定義 r, g, b 狀態，初始值為 128
     const [r, setR] = useState(128);
     const [g, setG] = useState(128);
     const [b, setB] = useState(128);
 
+    // 定義更新 r, g, b 狀態的函數
     const updateR = (v) => setR(v);
     const updateG = (v) => setG(v);
     const updateB = (v) => setB(v);
 
+    // 定義用於設置樣式的 CSS 對象
     const rgbCss = {
-        border: "1px solid blue",
-        borderRadius: "20px",
-        width: "300px",
-        display: "flex",
-        flexDirection: "column",
-        padding: "20px",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "auto",
-      };
-  return (
-    <>
-      <div
-        style={rgbCss}
-      >
-        <h2 style={{color: "red"}}>
-          目前色彩
-        </h2>
-        <div
-          style={{
-            width: "250px",
-            height: "100px",
-            backgroundColor: `rgb(${r}, ${g}, ${b})`,
-          }}
-        />
-        <span>R: </span>
-        <MySlider onChange={updateR} />
-        <span>G: </span>
-        <MySlider onChange={updateG} />
-        <span>B: </span>
-        <MySlider onChange={updateB} />
-      </div>
-    </>
-  );
+        border: "1px solid blue", // 藍色邊框
+        borderRadius: "20px", // 圓角半徑為 20px
+        width: "300px", // 寬度為 300px
+        display: "flex", // 使用 flex 佈局
+        flexDirection: "column", // 垂直排列
+        padding: "20px", // 內邊距為 20px
+        justifyContent: "center", // 垂直居中對齊
+        alignItems: "center", // 水平居中對齊
+        margin: "auto", // 自動外邊距
+    };
+
+    // 返回 MyRGBPanel 組件的 JSX 結構
+    return (
+        <>
+            <div style={rgbCss}>
+                <h2 style={{ color: "red" }}>目前色彩</h2>
+                <div
+                    style={{
+                        width: "250px",
+                        height: "100px",
+                        backgroundColor: `rgb(${r}, ${g}, ${b})`, // 背景色根據 r, g, b 動態變化
+                    }}
+                />
+                <span>R: </span>
+                {/* 渲染 MySlider 組件，傳遞 updateR 函數作為 onChange 屬性 */}
+                <MySlider onChange={updateR} />
+                <span>G: </span>
+                {/* 渲染 MySlider 組件，傳遞 updateG 函數作為 onChange 屬性 */}
+                <MySlider onChange={updateG} />
+                <span>B: </span>
+                {/* 渲染 MySlider 組件，傳遞 updateB 函數作為 onChange 屬性 */}
+                <MySlider onChange={updateB} />
+            </div>
+        </>
+    );
 }
 
+// 將 MyRGBPanel 組件導出，以便其他模組引入和使用
 export default MyRGBPanel;
--- src/components/MySlider.js
+++ src/components/MySlider.js
@@ -1,26 +1,35 @@
-import { useState } from "react";
+import { useState } from "react"; // 從 React 模組中引入 useState 鉤子
 
-function MySlider({onChange}) {
+// 定義 MySlider 組件，接收 onChange 函數作為參數
+function MySlider({ onChange }) {
+  // 使用 useState 定義 value 狀態，初始值為 128
   const [value, setValue] = useState(128);
+
+  // 定義 handleChange 函數，用於處理滑動條變化事件
   const handleChange = (e) => {
-    setValue(e.target.value);
-    if (onChange){
+    setValue(e.target.value); // 更新 value 狀態為滑動條的值
+    // 如果存在 onChange 函數，則調用該函數並傳入滑動條的值
+    if (onChange) {
       onChange(e.target.value);
     }
   };
+
+  // 返回滑動條組件的 JSX 結構
   return (
     <div>
       <input
         type="range"
-        width = "200"
+        width="200" // 這裡 width 屬性沒有效果，可移除
         min="0"
         max="255"
         value={value}
-        onChange={handleChange}
+        onChange={handleChange} // 滑動條變化時調用 handleChange 函數
       />
-      <span>{value}</span>
+      <span>{value}</span> {/* 顯示當前滑動條的值 */}
     </div>
   );
 }
 
+// 將 MySlider 組件導出，以便其他模組引入和使用
 export default MySlider;
+
